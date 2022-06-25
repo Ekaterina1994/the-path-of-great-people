98c11f66740c29dd1f6f398fdcce78645f97d19a
Alexei <marusovalexei@gmail.com>
2022-06-25 20:09:08 +0300

    feat: add dependencies and initial project structure

--- the-path-of-great-people/src/App.jsx
+++ the-path-of-great-people/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Info from "./info/info";
 const React = require("react");
 
 function App() {   
   return (
     <div className="App">
       hello!
+      <Info />
     </div>
   );
 }
