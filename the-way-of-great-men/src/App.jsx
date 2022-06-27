a50a4912bb709ae60f490bc0fbab19239637f22f
Ekaterina1994 <katishteno@mail.ru>
2022-06-27 23:13:18 +0300

    fix: rename root folder

---
+++ the-way-of-great-men/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Main from "./main/main";
+const React = require("react");
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ the-way-of-great-men/src/main/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./main.css";
+
+const Main = () => {
+  return (
+    <div>
+      <div className="img">1</div>
+    </div>
+  );
+};
+
+export default Main;
